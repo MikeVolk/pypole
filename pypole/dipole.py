b66a2212ed32a1e3cfa8680099839d71990e9565
Mike Volk <m.w.r.volk@uu.nl>
2022-11-09 15:15:19 +0100

    first working version

---
+++ pypole/dipole.py
@@ -0,0 +1,119 @@
+import numba
+import numpy as np
+from numpy.typing import NDArray
+from pypole.maps import get_random_sources, get_grid
+import itertools
+
+
+def synthetic_map(
+    n_sources: int = 100,
+    pixels: int = 100,
+    sensor_distance: float = 5e-6,
+    pixel_size: float = 1e-6,
+) -> NDArray:
+    """Calculate a single simple magnetic field map for a number of sources.
+
+    Parameters
+    ----------
+    n_sources : int
+        number of sources
+    pixels : int
+        number of pixels in the map
+    sensor_distance : float
+        distance from the sensor to the sample
+    pixel_size : float
+        size of each pixel in meters
+
+    Returns
+    -------
+    b_map : ndarray(pixels, pixels)
+        magnetic field map
+    """
+    # get the map grid in meters
+    x_grid, y_grid = get_grid(pixels, pixel_size)
+
+    # get the location for the sources
+    locations, source_vectors = get_random_sources(n_sources)
+    return calculate_map(x_grid, y_grid, locations, source_vectors, sensor_distance)
+
+def calculate_map(
+    x_grid: np.ndarray,
+    y_grid: np.ndarray,
+    locations: np.ndarray,
+    source_vectors: np.ndarray,
+    sensor_distance: float = 5e-6,
+) -> np.ndarray:
+    """Calculate the magnetic field map for a set of sources
+
+    Parameters
+    ----------
+    x_grid : ndarray(pixel, pixel)
+        x grid
+    y_grid : ndarray(pixel, pixel)
+        y grid
+    locations : ndarray(n_sources, 3)
+        x,y,z locations of sources
+    source_vectors : ndarray(n_sources, 3)
+        dipole moments in (x, y, z) format and Am2
+    sensor_distance : float, optional
+        sensor-sample distance by default 0.0
+    """
+
+    n_sources = locations.shape[0]
+    b = np.zeros((n_sources, x_grid.shape[0], x_grid.shape[1]))
+
+    for i in range(n_sources):
+        b[i,:,:] = dipole_field(x_grid, y_grid,
+                          locations[i, 0],
+                          locations[i, 1],
+                          locations[i, 2] + sensor_distance,
+                          source_vectors[i,0],
+                          source_vectors[i,1],
+                          source_vectors[i,1],
+                          )
+    return np.sum(b, axis=0)
+
+@numba.jit(fastmath=True)
+def dipole_field(
+    x_grid: np.ndarray,
+    y_grid: np.ndarray,
+    x_source: np.ndarray,
+    y_source: np.ndarray,
+    z_observed: np.ndarray,
+    mx: float,
+    my: float,
+    mz: float,
+) -> np.ndarray:
+    """Compute the field of a magnetic dipole point source
+
+    Parameters
+    ----------
+    x_source: ndarray (n_sources, 1)
+        x-locations of source
+    y_source:  ndarray (n_sources, 1)
+        y-locations of source
+    z_observed: ndarray(n_sources)
+        observed z distance, including the sensor height
+    x_grid:  ndarray(pixel, pixel)
+        grid to calculate the fields for
+    y_grid: ndarray(pixel, pixel)
+        grid to calculate the fields on
+    mx: ndarray(n_sources,)
+        x-component of vector
+    my: ndarray(n_sources,)
+        y-component of vector
+    mz: ndarray(n_sources,)
+        z-component of vector
+    """
+    dgridx = np.subtract(x_grid, x_source)
+    dgridy = np.subtract(y_grid, y_source)
+
+    squared_distance = np.square(dgridx) + np.square(dgridy) + np.square(z_observed)
+
+    aux = mx * dgridx + my * dgridy + mz * z_observed
+    aux /= np.sqrt(np.power(squared_distance, 5.0))
+    return 1e-7 * (
+        3.0 * aux * z_observed - mz / np.sqrt(np.power(squared_distance, 3.0))
+    )
+
+
--- pypole/maps.py
+++ pypole/maps.py
@@ -7,19 +7,16 @@ from pypole import convert
 
 
 def get_random_sources(
-    n_maps: int,
     n_sources: int,
     x_range: tuple = (-3.0e-6, 3.0e-6),
     y_range: tuple = (-3.0e-6, 3.0e-6),
-    z_range: tuple = (0, 3e-6),
+    z_range: tuple = (1e-6, 4e-6),
     moment_range: tuple = (1e-14, 1e-14),
 ) -> tuple[NDArray, NDArray]:
     """Generate diction of point source parameters
 
     Parameters
     ----------
-    n_maps: int
-             Number of maps to calculate
     n_sources: int
              Number of sources per map
     x_range:  tuple (min, max)
@@ -44,21 +41,21 @@ def get_random_sources(
     """
 
     # get the locations of the sources
-    locations = get_random_locations(n_maps, n_sources, x_range, y_range, z_range)
+    locations = get_random_locations(n_sources, x_range, y_range, z_range)
     # reshape total location to be (n_maps, n_sources, (x,y,z))
-    locations = locations.reshape(n_maps, n_sources, 3)
+    locations = locations.reshape(n_sources, 3)
 
     # calculate the x/y/z components
-    dim = get_random_dim(moment_range, n_maps, n_sources)
+    dim = get_random_dim(moment_range, n_sources)
     xyz = convert.dim2xyz(dim)
 
-    # reshape xyz to be (n_maps, n_sources, (x,y,z))
-    source_vector = xyz.reshape(n_maps, n_sources, 3)
+    # reshape xyz to be (n_sources, (x,y,z))
+    source_vector = xyz.reshape(n_sources, 3)
 
     return locations, source_vector
 
 
-def get_random_dim(moment_range, n_maps, n_sources):
+def get_random_dim(moment_range, n_sources):
     """ Generate randomly distributed dipole moments on the unit sphere
 
     Parameters
@@ -76,23 +73,21 @@ def get_random_dim(moment_range, n_maps, n_sources):
         dipole moments in (declination, inclination, moment) format
     """
     # get random declinations from uniform distribution (rand)
-    declination = np.random.uniform(0, 360, n_maps * n_sources)
+    declination = np.random.uniform(0, 360, n_sources)
     # get random inclinations from uniform distribution (rand)
-    inclination = np.rad2deg(np.arccos(2 * np.random.rand(n_maps * n_sources) - 1)) - 90
+    inclination = np.rad2deg(np.arccos(2 * np.random.rand(n_sources) - 1)) - 90
     # get uniform distribution of moment magnitude
     moment_scalar = np.random.uniform(
-        moment_range[0], moment_range[1], size=n_maps * n_sources
+        moment_range[0], moment_range[1], size=n_sources
     )
     return np.stack([declination, inclination, moment_scalar]).T
 
 
-def get_random_locations(n_maps, n_sources, x_range, y_range, z_range):
+def get_random_locations(n_sources, x_range, y_range, z_range):
     """Generate random locations for sources within the source region of the map (x_range, y_range, z_range)
 
     Parameters
     ----------
-    n_maps: int
-        number of maps to generate
     n_sources: int
         number of sources per map
     x_range: tuple (min, max)
@@ -109,13 +104,13 @@ def get_random_locations(n_maps, n_sources, x_range, y_range, z_range):
     """
 
     # get uniform distribution of x/y locations
-    x_source = np.random.uniform(x_range[0], x_range[1], size=(n_maps * n_sources))
-    y_source = np.random.uniform(y_range[0], y_range[1], size=(n_maps * n_sources))
-    z_source = np.random.uniform(z_range[0], z_range[1], size=(n_maps * n_sources))
+    x_source = np.random.uniform(x_range[0], x_range[1], size=n_sources)
+    y_source = np.random.uniform(y_range[0], y_range[1], size=n_sources)
+    z_source = np.random.uniform(z_range[0], z_range[1], size=n_sources)
     return np.stack([x_source, y_source, z_source]).T
 
 
-def get_grid(n_pixel: int = 100, pixel_size: float = 5e-6):
+def get_grid(n_pixel: int = 100, pixel_size: float = 5e-6) -> NDArray:
     """Generate observation coordinates of the map
 
     Parameters
