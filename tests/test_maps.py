b6e99704e071692245cdef855abd25fcae9e5ec6
Michael Volk <michaelvolk1979@gmail.com>
2022-11-22 12:02:28 +0100

    fix: ignore test files in mypy

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -1,9 +1,10 @@
-import pytest
-from pypole import maps
+# type: ignore
 import numpy as np
+import pytest
 
-@pytest.mark.parametrize("test_input,expected",
-                         [({(10,10),1}, np.array())])
+from pypole import maps
 
+
+@pytest.mark.parametrize("test_input,expected", [({(10, 10), 1}, np.array())])
 def test_eval(test_input, expected):
     assert maps(test_input) == expected
