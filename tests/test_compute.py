9185e20976317b555ca5ed3844632712fff8a689
Mike Volk <michaelvolk1979@gmail.com>
2023-04-01 16:05:06 +0200

    add test

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -42,3 +42,62 @@ def test_rms():
     expected_rms_nd = np.sqrt(np.mean(np.square(b_map_nd), axis=(-2,-1)))
     result_rms_nd = compute.rms(b_map_nd)
     np.testing.assert_almost_equal(result_rms_nd, expected_rms_nd)
+
+
+
+def test_dipolarity_param():
+    np.random.seed(0)
+
+    # Test for maps with shape (10, 10)
+    data_map_2d = np.random.randint(0, 100, size=(10, 10))
+    fitted_map_2d = np.random.randint(0, 100, size=(10, 10))
+    dp_2d = compute.dipolarity_param(data_map_2d, fitted_map_2d)
+
+    # Test for maps with shape (2, 10, 10)
+    data_map_3d = np.random.randint(0, 100, size=(2, 10, 10))
+    fitted_map_3d = np.random.randint(0, 100, size=(2, 10, 10))
+    dp_3d = compute.dipolarity_param(data_map_3d, fitted_map_3d)
+
+    # Test for maps with shape (2, 3, 4, 10, 10)
+    data_map_5d = np.random.randint(0, 100, size=(2, 3, 4, 10, 10))
+    fitted_map_5d = np.random.randint(0, 100, size=(2, 3, 4, 10, 10))
+    dp_5d = compute.dipolarity_param(data_map_5d, fitted_map_5d)
+
+    # Check that dipolarity parameters are within the valid range [0, 1]
+    assert 0 <= dp_2d <= 1
+    assert np.all((0 <= dp_3d) & (dp_3d <= 1))
+    assert np.all((0 <= dp_5d) & (dp_5d <= 1))
+
+    # Test for mismatched shapes
+    with pytest.raises(ValueError):
+        compute.dipolarity_param(data_map_2d, fitted_map_3d)
+
+
+def test_pad_map():
+
+    # Test 2D maps
+    b_map = np.random.randint(0, 10, size=(3, 3))
+    padded = compute.pad_map(b_map, oversample=2)
+    assert padded.shape == (9,9)
+    assert np.array_equal(padded[3:6, 3:6], b_map)
+
+    b_map = np.random.randint(0, 10, size=(4, 3))
+    padded = compute.pad_map(b_map, oversample=3)
+    assert padded.shape == (20, 15)
+    assert np.array_equal(padded[8:8+4, 6:6+3], b_map)
+
+    # Test ND maps
+    b_map = np.random.randint(0, 10, size=(2, 3, 3))
+    padded = compute.pad_map(b_map, oversample=2)
+    assert padded.shape == (2, 9, 9)
+    assert np.array_equal(padded[:, 3:6, 3:6], b_map)
+
+    b_map = np.random.randint(0, 10, size=(2, 3, 4))
+    padded = compute.pad_map(b_map, oversample=3)
+    assert padded.shape == (2, 15, 20)
+    assert np.array_equal(padded[:, 6:6+3, 8:8+4], b_map)
+
+    b_map = np.random.randint(0, 10, size=(2, 3, 4, 5, 6))
+    padded = compute.pad_map(b_map, oversample=4)
+    assert padded.shape == (2, 3, 4, 35, 42)
+    assert np.array_equal(padded[:, :, :, 15:15+5, 18:18+6], b_map)
